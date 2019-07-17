814f45f4f43ce1d3a08180e7bcba6d2556d301b5
Jiaqi Jiang <jiangjq@stanford.edu>
2019-07-16 18:11:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ eng.addpath(eng.genpath('/reticolo_allege'));
 eng.addpath(eng.genpath('solvers'));
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--output_dir', default='Results', help="Generated devices folder")
+parser.add_argument('--output_dir', default='results', help="Generated devices folder")
 parser.add_argument('--restore_from', default=None, help="Optional, directory or file containing weights to reload before training")
 parser.add_argument('--wavelength', default=900)
 parser.add_argument('--angle', default=60)
